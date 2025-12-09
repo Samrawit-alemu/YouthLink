54a2237abc90b816fef07442387f1a45413be6df
Samrawit-alemu <samrawittalemuu@gmail.com>
2025-12-09 11:20:39 -0800

    Created User Schema

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    // 1. The link to Firebase
+    firebaseUid: {
+        type: String,
+        required: true,
+        uniqur: true
+    },
+    email: {
+        type: String,
+        required: true,
+        uniqur: true
+    },
+    role: {
+        type: String,
+        enum: ['jobseeker', 'employer'],
+        required: true
+    },
+    // 2. common profile info
+    name: {
+        type: String,
+        required: true
+    },
+    // 3. Job seeker fields
+    skills: {
+        type: [String],
+        default: []
+    },
+    bio: {
+        type: String
+    },
+    portfolioUrl: {
+        type: String
+    },
+    // 4. Employer fields
+    companyName: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    phone: {
+        type: String
+    }
+}, { timestamps: true }) // Automatically adds 'createdAt' and 'updatedAt'
+
+module.exports = mongoose.model('User', UserSchema)
